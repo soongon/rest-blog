1f17bc33f39e1953be0feb82a32e8ad68cbf8f16
KITRI <soongon@gmail.com>
2024-06-26 15:50:53 +0900

    spring data jpa 로 변환

--- src/main/java/kr/re/kitri/restblog/model/Article.java
+++ src/main/java/kr/re/kitri/restblog/model/Article.java
@@ -1,15 +1,20 @@
 package kr.re.kitri.restblog.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
-@Table("article")
+@Entity
 public class Article {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private String content;
--- src/main/java/kr/re/kitri/restblog/repository/ArticleRepository.java
+++ src/main/java/kr/re/kitri/restblog/repository/ArticleRepository.java
@@ -1,13 +1,14 @@
 package kr.re.kitri.restblog.repository;
 
+import jakarta.transaction.Transactional;
 import kr.re.kitri.restblog.model.Article;
-import org.springframework.data.jdbc.repository.query.Modifying;
-import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ArticleRepository extends CrudRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long> {
 
     @Modifying
-    @Query("UPDATE public.article SET views = views + 1 WHERE id = :id")
+    @Query(value = "UPDATE public.article SET views = views + 1 WHERE id = :id", nativeQuery = true)
     void increaseViewCount(long id);
 }
